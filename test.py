15ce35488304fc509e1dd969991edac878ff703b
Radek Vasicek <radekvr@gmail.com>
2026-03-11 10:50:28 +0100

    changes on the file plotting and normalization of the intensity

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ import helpers as hs
 
 
 theta, phi, nx, ny, nz = hs.make_angle_grid(n_theta=241, n_phi=481)
-npz = np.load('./sim_test6.npz')
+npz = np.load('./sim_test_paramssetup.npz')
 
 #['intensity', 'atom_pos', 'w', 'pcenter']
 
@@ -21,9 +21,23 @@ I = npz['intensity']
 times = npz['times']
 T = times.shape[0]
 
+
+rplotting.plot_pattern_3d(nx,ny,nz, I[0])
+
 #ani = rplotting.animation_atoms_with_pulse(pos,times.shape[0],w, pulsecenter)
 
-ani1 = rplotting.animate_pattern_3d(nx,ny,nz, I)
+# normalize relative to the global max over all entries
+#I_max = np.max(I)
+#if I_max > 0:
+#    I = I / I_max
+
+
+# normalize each frame by its own max
+frame_max = np.max(I, axis=tuple(range(1, I.ndim)), keepdims=True)
+frame_max[frame_max == 0] = 1.0
+I = I / frame_max
+
+#ani1 = rplotting.animate_pattern_3d(nx,ny,nz, I)
 
 
 plt.show()
