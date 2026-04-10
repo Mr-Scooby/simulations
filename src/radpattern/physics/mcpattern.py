d98048260313cc2e31596df2a94a8ffde78a1c0d
Radek Vasicek <radekvr@gmail.com>
2026-04-10 11:06:40 +0200

    added/ fixed montecarlo engine for the static case

--- src/radpattern/physics/mcpattern.py
+++ src/radpattern/physics/mcpattern.py
@@ -9,6 +9,14 @@ import time
 import logging
 import numpy as np
 
+
+from radpattern.geometry.cloud_model import CloudModel
+from radpattern.geometry.grids import AngleGrid
+from radpattern.physics.beam import BeamModel
+from radpattern.physics.rpattern import array_factor_general
+
+
+
 from radpattern.helpers.helpers import (
     make_angle_grid,
     gaussian_weights,
@@ -25,90 +33,62 @@ from radpattern.helpers.helpers import (
 log = logging.getLogger(__name__)
 
 # ---------------------------------------------------------------------
-# Core physics
+#def compute_realization_af_series_ballistic(
+#    cloud, 
+#    beam, 
+#    grid,
+#    times: np.ndarray,
+#    chunk_atoms: int = 2000,
+#    **kwargs
+#) -> np.ndarray:
+#    """
+#    Compute AF(t, theta, phi) for one realization using ballistic motion:
+#        r_j(t) = r0_j + v_j t
+#
+#    We precompute, per chunk:
+#        phi_r0 = k_out * (n_hat · r0)
+#        phi_v  = k_out * (n_hat · v)
+#
+#    Then at each time:
+#        phase(t) = phi_r0 + t * phi_v
+#
+#    Notes
+#    -----
+#    - w_fn must be atom-local, since it is called chunk by chunk.
+#    - Returns complex AF series with shape (T, nt, np_).
+#    """
+#    times = np.asarray(times, dtype=float)
+#    nt, np_ = grid.shape
+#    T = times.size
+#    n_atoms = r0_xyz.shape[0]
+#    
+#    log.info("computing AF ... ") 
+#    AF_series = np.zeros((T, nt * np_), dtype=np.complex128)
+#
+#    for a0 in range(0,n_atoms, chunk_atoms):
+#        a1 = min(a0 + chunk_atoms, n_atoms)
+#
+#        r0_chunk = np.asarray(r0_xyz[a0:a1], dtype=float)   # (C, 3)
+#        v_chunk  = np.asarray(v_xyz[a0:a1], dtype=float)    # (C, 3)
+#
+#        # Precompute ballistic phase ingredients for this chunk
+#        phi_r0 = k_out * (grid.n_hat_flat @ r0_chunk.T)          # (M, C)
+#        phi_v  = k_out * (grid.n_hat_flat @ v_chunk.T)           # (M, C)
+#
+#        for it, t in enumerate(times):
+#            rt_chunk = r0_chunk + t * v_chunk              # (C, 3)
+#            wt_chunk = np.asarray(w_fn(rt_chunk, t, return_pulse_center = kwargs.get('return_pulse_center') ), dtype=np.complex128)  # (C,)
+#
+#            AF_series[it] += np.exp(1j * (phi_r0 + t * phi_v)) @ wt_chunk
+#
+#    
+#    return AF_series.reshape(T, nt, np_)
+#
 
-def positions_at_time(r0_xyz: np.ndarray, v_xyz: np.ndarray, t: float) -> np.ndarray:
-    """Ballistic motion: r(t) = r0 + v*t."""
-    r0_xyz = np.asarray(r0_xyz, dtype=float)
-    v_xyz = np.asarray(v_xyz, dtype=float)
-
-    log.debug("Update position. time: %f", t)
-    return r0_xyz + v_xyz * float(t)
-
-def sample_realization(n_atoms, rng, **kwargs):
-    """Sample random initial positions and velocities for one MC realization."""
-    # Sample one realization of initial positions and velocities.
-    log.info("Generating random sample.")
-    r0_xyz = np.asarray(random_position(n_atoms, seed=int(rng.integers(0, 2**32)),
-                                        box_size = kwargs.get("box_size"), center = kwargs.get("center"),
-                                        plane_restricted = kwargs.get("plane_restricted")
-                                        ) , dtype=float)
-    v_xyz = np.asarray(
-        random_velocity_thermal(r0_xyz,seed=int(rng.integers(2**32)),
-                                v_std = kwargs.get("v_std"), plane_restricted = kwargs.get("plane_restricted")),dtype=float)
-
-    return r0_xyz, v_xyz
-
-def compute_realization_af_series_ballistic(
-    n_hat_flat: np.ndarray,
-    grid_shape,
-    k_out: float,
-    times: np.ndarray,
-    r0_xyz: np.ndarray,
-    v_xyz: np.ndarray,
-    w_fn,
-    chunk_atoms: int = 2000,
-    **kwargs
-) -> np.ndarray:
-    """
-    Compute AF(t, theta, phi) for one realization using ballistic motion:
-        r_j(t) = r0_j + v_j t
-
-    We precompute, per chunk:
-        phi_r0 = k_out * (n_hat · r0)
-        phi_v  = k_out * (n_hat · v)
-
-    Then at each time:
-        phase(t) = phi_r0 + t * phi_v
-
-    Notes
-    -----
-    - w_fn must be atom-local, since it is called chunk by chunk.
-    - Returns complex AF series with shape (T, nt, np_).
-    """
-    times = np.asarray(times, dtype=float)
-    nt, np_ = grid_shape
-    T = times.size
-    n_atoms = r0_xyz.shape[0]
-    
-    log.info("computing AF ... ") 
-    AF_series = np.zeros((T, nt * np_), dtype=np.complex128)
-
-    for a0 in range(0, n_atoms, chunk_atoms):
-        a1 = min(a0 + chunk_atoms, n_atoms)
-
-        r0_chunk = np.asarray(r0_xyz[a0:a1], dtype=float)   # (C, 3)
-        v_chunk  = np.asarray(v_xyz[a0:a1], dtype=float)    # (C, 3)
-
-        # Precompute ballistic phase ingredients for this chunk
-        phi_r0 = k_out * (n_hat_flat @ r0_chunk.T)          # (M, C)
-        phi_v  = k_out * (n_hat_flat @ v_chunk.T)           # (M, C)
-
-        for it, t in enumerate(times):
-            rt_chunk = r0_chunk + t * v_chunk              # (C, 3)
-            wt_chunk = np.asarray(w_fn(rt_chunk, t, return_pulse_center = kwargs.get('return_pulse_center') ), dtype=np.complex128)  # (C,)
-
-            AF_series[it] += np.exp(1j * (phi_r0 + t * phi_v)) @ wt_chunk
-
-    
-    return AF_series.reshape(T, nt, np_)
-
-
-def compute_realization_intensity_series(n_hat_flat, grid_shape, dipole:np.ndarray,  k_out: float,
+def compute_realization_intensity_series(grid, dipole:np.ndarray,  k_out: float,
                                         p_hat: np.ndarray, times: np.ndarray,
                                          n_atoms:int,rng:np.random, w_fn,
                                          chunk_atoms: int = 20000,
-                                        normalize_each_time: bool = False,
                                          **kwargs
                                         ) -> np.ndarray:
     """
@@ -136,8 +116,6 @@ def compute_realization_intensity_series(n_hat_flat, grid_shape, dipole:np.ndarr
         Weight function w_fn(r_t, t) for this realization. If None, unit weights are used.
     chunk_atoms : int, optional
         Number of atoms per chunk in the array factor calculation.
-    normalize_each_time : bool, optional
-        If True, normalize each time snapshot by its own maximum.
 
     Returns
     -------
@@ -148,7 +126,7 @@ def compute_realization_intensity_series(n_hat_flat, grid_shape, dipole:np.ndarr
     # time array
     times = np.asarray(times, dtype=float)
     T = times.size
-    nt, np_ = grid_shape
+    nt, np_ = grid.shape
 
     # Sample radom atoms and velocity
     r0_xyz, v_xyz = sample_realization(n_atoms,rng, **kwargs)
@@ -157,13 +135,12 @@ def compute_realization_intensity_series(n_hat_flat, grid_shape, dipole:np.ndarr
     I_series = np.zeros((T, nt, np_), dtype=float)
 
     log.info(
-        "compute_realization_intensity_series : N=%d, T=%d, grid=(%d,%d), chunk_atoms=%d, normalize_each_time=%s",
-        len(r0_xyz), T, nt, np_, chunk_atoms, normalize_each_time
+        "compute_realization_intensity_series : N=%d, T=%d, grid=(%d,%d), chunk_atoms=%d ",
+        len(r0_xyz), T, nt, np_, chunk_atoms, 
     )
 
     AF_series = compute_realization_af_series_ballistic(
-        n_hat_flat=n_hat_flat,
-        grid_shape = grid_shape,
+        grid ,
         k_out=k_out,
         times=times,
         r0_xyz=r0_xyz,
@@ -185,14 +162,72 @@ def compute_realization_intensity_series(n_hat_flat, grid_shape, dipole:np.ndarr
 
 
 
-def mc_sim( nx,ny,nz, grid_shape,
+def static_AF_calculation(cloud, beam, grid, rng = None): 
+    """ Single shot computation of the cloud and beam """ 
+
+    r_xyz = cloud.make_positions(rng)
+    w = beam.generate_weights(r_xyz, t=0.0)
+    return array_factor_general(
+        n_hat_flat=grid.n_hat_flat,
+        grid_shape=grid.shape,
+        k_out= beam.k_in,
+        r_xyz=r_xyz,
+        w=w,
+        )
+
+grid = AngleGrid()
+
+def mc_static(cloud, beam, grid, runs, seed = 0): 
+
+    log.info("mc_sim: runs=%d",runs )
+
+    AF2_acc = np.zeros(grid.shape, dtype=float)
+    AF_acc = np.zeros(grid.shape, dtype=complex)
+
+    # Random generator object, helps to reproduce runs. 
+    rng_master = np.random.default_rng(seed)
+    # time to measure how long it takes. 
+    t_start = time.time()
+
+    # Runs of the simulation
+    log.info("Starting mc runs ... 1 /%d", runs)
+    for mc in range(runs ):
+        # each simulation time keeper
+        t_mc = time.time()
+        # Independent RNG for this realization.
+        rng = np.random.default_rng(rng_master.integers(0, 2**32))
+
+        AF_mc = static_AF_calculation(cloud, beam, grid, rng= rng)
+        # Intensity acumalation. 
+        AF_acc += AF_mc
+        AF2_acc += np.abs(AF_mc)**2
+
+        log.info(
+            "mc run %d/%d done in %.2fs",
+            mc + 1, runs, time.time() - t_mc
+        )
+    AF2_mean = AF2_acc / float(runs)
+    AF_mean = AF_acc / float(runs)
+
+    log.info(
+        "mc_intensity_time_series end: total=%.2fs, avg_per_mc=%.2fs",
+        time.time() - t_start,
+        (time.time() - t_start) / max(float(runs), 1.0),
+    )
+
+    return AF_mean, AF2_mean
+
+
+
+
+
+def mc_sim( grid,
     k_out: float, p_hat: np.ndarray,
     times: np.ndarray,
     n_mc: int,
     n_atoms: int, 
     w_fn,
     chunk_atoms: int = 20000, seed: int = 0,
-    normalize_each_time: bool = False,
     **kwargs
 ) -> np.ndarray:
     """
@@ -204,8 +239,7 @@ def mc_sim( nx,ny,nz, grid_shape,
 
     Parameters
     ----------
-    nx, ny, nz : np.ndarray
-        Direction cosine grids for the observation directions.
+    grid: grid obejct
     k_out : float
         Output wave number.
     p_hat : np.ndarray
@@ -220,9 +254,7 @@ def mc_sim( nx,ny,nz, grid_shape,
         Chunk size used in the array factor calculation.
     seed : int, optional
         Master seed for reproducible Monte Carlo sampling.
-    normalize_each_time : bool, optional
-        If True, normalize each time snapshot by its own maximum before averaging.
-    w_fn_factory : callable or None, optional
+        w_fn_factory : callable or None, optional
         Callable with signature w_fn_factory(rng) -> w_fn(r_t, t).
         If None, unit weights are used.
 
@@ -231,20 +263,18 @@ def mc_sim( nx,ny,nz, grid_shape,
     np.ndarray
         Monte Carlo averaged intensity series with shape (T, n_theta, n_phi).
     """
-
+ 
     times = np.asarray(times, dtype=float)
     T = times.size
     nt, np_ = grid_shape 
 
     log.info(
         "mc_sim: n_mc=%d, n_atoms=%d, T=%d, grid=(%d,%d), "
-        "chunk_atoms=%d,  normalize_each_time=%s",
+        "chunk_atoms=%d ",
         n_mc, n_atoms, T, nt, np_, chunk_atoms, 
-         normalize_each_time,
     )
 
     # Flatten directions: n_hat_flat (M,3), M=nt*np
-    n_hat_flat = np.stack([nx, ny, nz], axis=-1).reshape(-1, 3)
     dipole = single_dipole_E(nx,ny,nz, p_hat) 
 
     I_accum = np.zeros((T, nt, np_), dtype=float)
@@ -264,7 +294,7 @@ def mc_sim( nx,ny,nz, grid_shape,
         rng = np.random.default_rng(rng_master.integers(0, 2**32))
 
         # Simulation
-        I_mc = compute_realization_intensity_series(n_hat_flat= n_hat_flat,
+        I_mc = compute_realization_intensity_series(n_hat_flat= grid.n_hat_flat,
                 grid_shape = grid_shape, 
                 dipole= dipole, 
                 k_out=k_out,
@@ -273,7 +303,6 @@ def mc_sim( nx,ny,nz, grid_shape,
                 n_atoms = n_atoms,
                 rng=rng,
                 chunk_atoms=chunk_atoms,
-                normalize_each_time=normalize_each_time,
                 w_fn=w_fn, **kwargs
         )
 
